5058d1135ddbc6d56422bfad96da104fae128628
bdemeshev <boris.demeshev@gmail.com>
2022-01-20 19:12:00 +0300

    l2 working on.

--- lecture_02.R
+++ lecture_02.R
@@ -50,9 +50,76 @@ gg_tsdisplay(d, y)
 # sample Corr(y_t, y_{t-k}) versus k
 ACF(d, y)
 # sample Corr(y_t, y_{t-6}) = -0.165
+# ts cut by year
+# no seasonal effects
+
+# github.com/bdemeshev/icef_r4finance_2022
 
 
+gg_lag(d, y)
+# y_t versus y_{t-1}
+# y_t versus y_{t-2}
+# ...
+gg_lag(d, b)
 
 
+gg_subseries(d, y)
+gg_season(d, y)
 
+# ARMA, ARIMA model? (yes)
+# ETS (exponential smoothing) model? (no)
+# THETA method? (no)
+
+# SARIMA(1,1,1)-(2,0,1)[12]
+
+# ARIMA(1, 0, 2)
+# (1 - a_1 L) (y_t - mu) = (1 + b_1 L + b_2 L^2) u_t
+# u_t ~ white noise, N(0; sigma^2)
+
+# y_t ~ ARIMA(1, 1, 2)     <=>   Delta y_t ~ ARIMA(1, 0 ,2)
+# (1 - a_1 L) (1 - L) y_t = (1 + b_1 L + b_2 L^2) u_t
+# u_t ~ white noise, N(0; sigma^2)
+# (1 - L)y_t = Delta y_t
+
+# SARIMA(1,1,1)-(2,0,1)[12]: (non-seas)-(seasonal)[periodicity]
+# (1,1,1): 1 coef in AR, 1 order of difference, 1 in MA
+# (2,0,1): 2 coefs in SAR, 0 order of seas diff, 1 in SMA
+# (1 - c_1 L^12 - c_2 L^24)(1 - a_1 L) (1 - L) y_t =
+# = (1 + b_1 L) (1 + d_1 L^12) u_t
+# R will estimate c_1, c_2, a_1, b_1, d_1, sigma^2
+
+
+d
+
+nrow(d)
+tail(d, 10)
+
+train = filter(d, date < ymd('2007-12-01'))
+tail(train)
+nrow(train)
+
+# you specify the model
+
+# automatic ARIMA selection: Khandakar-Hyndman procedure
+# 1. KPSS test to check whether we need to go from y_t to Delta y_t
+# done twice
+# 2. Seasonality "force" is calculated.
+# We check whether we need to go from y_t to (1 - L^12) y_t
+# STL decomposition is used
+# done twice
+# 3. A pack of stationary ARMA models is estimated
+# the best is selected via AIC
+# Forecasting principles and practice, Hyndman
+
+# ARMA(1,1) = SARIMA(1, 0, 1)-(0, 0, 0)[12]
+# automatic one
+
+mod_y = model(d,
+      arma11 = ARIMA(y ~ 1 + pdq(1, 0, 1) + PDQ(0, 0, 0)),
+      auto = ARIMA(y))
+mod_y
+report(mod_y$arma11[[1]])
+# (1 - (-0.3424)L) (y_t - (-0.0137)) = (1 + 0.1780L) u_t
+
+report(mod_y$auto[[1]])
 
