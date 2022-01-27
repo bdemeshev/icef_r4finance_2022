080e6313f516b6ec36deb32ed62e764d1aedbfb3
bdemeshev <boris.demeshev@gmail.com>
2022-01-27 20:23:27 +0300

    week3....

--- lecture_03.R
+++ lecture_03.R
@@ -18,4 +18,25 @@ mods = model(aus_train,
     auto_ets = ETS(Electricity),
     ets_log = ETS(log(Electricity)))
 
+# github.com/bdemeshev/icef_r4finance_2022
+report(mods$ets_aaa[[1]])
+# b_t = b_{t-1} + 0.05039968 u_t, b_0 = -1.993292
+# u_t ~ iid N(0; 98008.68)
+# s_t = s_{t-4} + 0.5352046 u_t, s_0 ...
+# ell_t = ell_{t-1} + b_{t-1} + 0.4036547  u_t, ell_0 = 4487.557
+# y_t = ell_{t-1} + b_{t-1} + s_{t-4} + u_t
+
+fcsts = forecast(mods, h = '4 years')
+fcsts
+
+aaa_fcsts = filter(fcsts, .model == 'ets_aaa')
+autoplot(aaa_fcsts, aus_train)
+
+report(mods$auto_ets[[1]])
+
+
+mam_fcsts = filter(fcsts, .model == 'auto_ets')
+autoplot(mam_fcsts, aus_train)
+
+
 
