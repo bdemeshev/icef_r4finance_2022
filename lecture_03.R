1aff8b93d31f368a430940328243708991829788
bdemeshev <boris.demeshev@gmail.com>
2022-01-27 20:12:37 +0300

    week3...

--- lecture_03.R
+++ lecture_03.R
@@ -2,3 +2,20 @@ library(tidyverse) # data manipulations
 library(rio) # import/export
 library(fpp3) # forecasting
 
+aus_production
+
+autoplot(aus_production, Electricity)
+
+aus_train = filter(aus_production, Quarter <= yearquarter('1980Q1'))
+autoplot(aus_train, Electricity)
+
+mods = model(aus_train,
+    ets_aaa = ETS(Electricity ~ error('A') + trend('A') + season('A')),
+    ets_ana = ETS(Electricity ~ error('A') + trend('N') + season('A')),
+    snaive = SNAIVE(Electricity),
+    auto_arima = ARIMA(Electricity),
+    ets_azz = ETS(Electricity ~ error('A')), # trend = A, Ad, M, Md, N; season = A, M, N
+    auto_ets = ETS(Electricity),
+    ets_log = ETS(log(Electricity)))
+
+
