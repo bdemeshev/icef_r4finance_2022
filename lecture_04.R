1492adc5bb9fe5397caa3acfd35b405e020d2bce
bdemeshev <boris.demeshev@gmail.com>
2022-02-03 18:44:44 +0300

    4.

---
+++ lecture_04.R
@@ -0,0 +1,9 @@
+# week 4
+# principal component analysis, cluster analysis, r + markdown
+
+library(tidyverse) # data manipulation
+library(rio) # data import/export
+library(cluster) # :)
+library(factoextra) # visualizations for pca, cluster...
+library(corrplot) # visualize correlations
+
