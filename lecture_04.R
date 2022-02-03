79309b04852002ff223aeaecfb2c2b527e2077a6
bdemeshev <boris.demeshev@gmail.com>
2022-02-03 19:29:58 +0300

    4..

--- lecture_04.R
+++ lecture_04.R
@@ -7,3 +7,61 @@ library(cluster) # :)
 library(factoextra) # visualizations for pca, cluster...
 library(corrplot) # visualize correlations
 
+# github.com/bdemeshev/icef_r4finance_2022
+# the script is there!
+
+# Handbook of Stat Analysis using R
+?HSAUR::heptathlon
+
+h = HSAUR::heptathlon
+glimpse(h)
+
+cor_mat = cor(h) # correlation
+corrplot(cor_mat)
+
+corrplot(cor_mat, order = 'hclust')
+
+corrplot(cor_mat, order = 'hclust', addrect = 3)
+
+str(h)
+
+h7 = select(h, -score)
+h7
+
+# principal component analysis?
+
+
+# we standardize original variables (!)
+# we create new variables (artificial) as linear combinations of old variables
+# max variance view: new variables (principal components) have maximal variance
+# max average R^2 view: new variables have maximal average R^2 if you predict
+# every old variable using new variables.
+
+
+?prcomp
+
+comps = prcomp(h7, scale. = TRUE)
+comps$x # new variables
+
+fviz_pca_ind(comps)
+
+# hurdles_i = a + b pc1_i: R^2_1
+# highjump_i = a + b pc1_i: R^2_2
+# ...
+# run800m_i = a + b pc1_i: R^2_7
+# 0.637 = (R^2_1 + ... + R^2_7) / 7
+
+# hurdles_i = a + b pc1_i + c pc2_i: R^2_1
+# highjump_i = a + b pc1_i + c pc2_i: R^2_2
+# ...
+# run800m_i = a + b pc1_i + c pc2_i: R^2_7
+# 0.637 + 0.171 = (R^2_1 + ... + R^2_7) / 7
+
+
+fviz_pca_ind(comps, repel = TRUE)
+
+fviz_pca_biplot(comps, repel = TRUE)
+
+comps$rotation
+
+fviz_eig(comps, addlabels = TRUE)
